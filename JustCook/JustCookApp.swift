364ae0f1052d91249f9417d05ea35dce31487904
eyanyehya <eyanyehya@gmail.com>
2022-07-14 12:33:06 +0300

    Initial Commit

---
+++ JustCook/JustCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustCookApp.swift
+//  JustCook
+//
+//  Created by Eyan Yehya on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct JustCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
